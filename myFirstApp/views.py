7af17582ec30751b279d40339cd0fd38c4d06ac5
Khalid Zia Rabbani <khalidziarabbani0473@gmail.com>
2024-02-13 17:49:35 +0700

    Remove unused imports and update user model in views.py

--- myFirstApp/views.py
+++ myFirstApp/views.py
@@ -1,12 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login, authenticate, logout
-from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.decorators import login_required
-from django.contrib.messages import get_messages
 from django.contrib import messages
-from django.contrib.auth.hashers import make_password
 
 from .models import Profile
 
