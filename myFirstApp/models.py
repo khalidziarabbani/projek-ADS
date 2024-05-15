ad3867a8f54dd992396b40277704ddda768ab771
Khalid Zia Rabbani <khalidziarabbani0473@gmail.com>
2024-05-15 16:38:32 +0700

    [fix regist form views.py, edit user models, add signals.py, edit apps.py]

--- myFirstApp/models.py
+++ myFirstApp/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
 class Profile(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE, default=None)  # Menambahkan default
+    phone_number = models.CharField(max_length=15, default='-')
+    full_name = models.CharField(max_length=255, default='Anonymous')
 
     def __str__(self):
-        return self.name
+        return self.user.username
--- myFirstApp/views.py
+++ myFirstApp/views.py
@@ -32,6 +32,8 @@ def register(request):
     if request.method == 'POST':
         username = request.POST['username']
         email = request.POST['email']
+        full_name = request.POST['full_name']
+        phone_number = request.POST['phone_number']
         password = request.POST['password']
         password2 = request.POST['password2']
 
@@ -43,10 +45,20 @@ def register(request):
             messages.error(request, 'Email already in use')
             return render(request, 'register.html')
 
+        if User.objects.filter(username=username).exists():
+            messages.error(request, 'Username already in use')
+            return render(request, 'register.html')
+
+        # Periksa apakah pengguna sudah memiliki profil
         user = User.objects.create_user(username=username, email=email, password=password)
+        if not Profile.objects.filter(user=user).exists():
+            Profile.objects.create(user=user, phone_number=phone_number, full_name=full_name)
+        else:
+            messages.error(request, 'Profile already exists for this user')
+
         login(request, user)
         messages.success(request, 'Registration successful. You are now logged in.')
-        return redirect('index')
+        return redirect('login')
 
     elif request.method == 'GET':
         return render(request, 'register.html')
