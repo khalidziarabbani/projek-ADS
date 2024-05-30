f593778dd85122768aedc5b5098bd085c7d7ce8c
Khalid Zia Rabbani <khalidziarabbani0473@gmail.com>
2024-05-30 23:54:00 +0700

    Update CSS styles for home page, category page, and base template

--- myFirstApp/static/js/product_detail.js
+++ myFirstApp/static/js/product_detail.js
@@ -1,21 +1,33 @@
 function toggleDescription(button) {
-    removeActiveClass();
+    document.getElementById('description').classList.add('visible');
     document.getElementById('description').classList.remove('hidden');
     document.getElementById('specification').classList.add('hidden');
+    document.getElementById('specification').classList.remove('visible');
+    
     button.classList.add('active');
+    document.getElementById('specificationBtn').classList.remove('active');
 }
 
 function toggleSpecification(button) {
-    removeActiveClass();
-    document.getElementById('description').classList.add('hidden');
+    document.getElementById('specification').classList.add('visible');
     document.getElementById('specification').classList.remove('hidden');
+    document.getElementById('description').classList.add('hidden');
+    document.getElementById('description').classList.remove('visible');
+    
     button.classList.add('active');
-
+    document.getElementById('descriptionBtn').classList.remove('active');
 }
 
-function removeActiveClass() {
-    var buttons = document.querySelectorAll('button');
-    buttons.forEach(function(btn) {
-        btn.classList.remove('active');
-    });
+function increaseQuantity() {
+    const quantityInput = document.getElementById('quantityInput');
+    let currentValue = parseInt(quantityInput.value);
+    quantityInput.value = currentValue + 1;
+}
+
+function decreaseQuantity() {
+    const quantityInput = document.getElementById('quantityInput');
+    let currentValue = parseInt(quantityInput.value);
+    if (currentValue > 1) {
+        quantityInput.value = currentValue - 1;
+    }
 }
